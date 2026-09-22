cc8b864e371f6eccd0d5d19f20ea4c337d9a9f1b
muhheryagungnugraha <nugraha2100018509@webmail.uad.ac.id>
2026-09-22 12:41:34 +0700

    edit 2004 to 2008

--- resources/views/sections/hero.blade.php
+++ resources/views/sections/hero.blade.php
@@ -4,9 +4,9 @@
         <div class="carousel-inner">
             <x-carousel-header-item
                 :src="Vite::image('carousel-header-item-1.jpg')"
-                badge="KONTRAKTOR KUBAH MASJID (EST. 2004)"
+                badge="KONTRAKTOR KUBAH MASJID (EST. 2008)"
                 title="Mahakarya Kubah Masjid Bergaransi 20 Tahun"
-                subtitle="Kami membangun kubah masjid yang bukan sekadar indah, tetapi kokoh, presisi, dan abadi. Didukung teknologi oven enamel, standar material pabrik, dan tim ahli berpengalaman sejak 2004."
+                subtitle="Kami membangun kubah masjid yang bukan sekadar indah, tetapi kokoh, presisi, dan abadi. Didukung teknologi oven enamel, standar material pabrik, dan tim ahli berpengalaman sejak 2008."
                 class="active" />
 
             <x-carousel-header-item
